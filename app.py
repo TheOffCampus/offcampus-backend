c164f71f56a8e788a12c9aba46d177d89b7f5c64
prithvi-patel-tamu <prith.vi@tamu.edu>
2024-02-28 14:03:52 -0600

    rpc impl

--- app.py
+++ app.py
@@ -7,37 +7,19 @@ app = Flask(__name__)
 
 SUPABASE_URL='https://ihnradjuxnddmmioyeqp.supabase.co/'
 SUPABASE_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlobnJhZGp1eG5kZG1taW95ZXFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc5NDkyODcsImV4cCI6MjAyMzUyNTI4N30.cebjm2GbItaRLa81OYTi3Suffy8u52hO3lSRgjrK5r8'
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:YOUR_PASSWORD@aws-0-us-west-1.pooler.supabase.com:5432/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:oMbKlcQqT9APDtKG@aws-0-us-west-1.pooler.supabase.com:5432/postgres"
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 def get_recs_query(prefs):
-    query = f"""
-        SELECT
-            p.id AS property_id,
-            p.data AS property_data,
-            rental_object,
-            (
-                ({ prefs["miles_weight"] } * 1000 * (c.college->>'miles')::float) +
-                ({ prefs["sqft_weight"] } * 800 * (rental_object->>'squareFeet')::int) +
-                ({ prefs["rent_weight"] } * (rental_object->>'rent')::int)
-            ) AS weighted_score
-        FROM
-            properties p,
-            jsonb_array_elements(p.data->'schools'->'colleges') AS c(college)
-        CROSS JOIN LATERAL
-            jsonb_array_elements(p.data->'rentals') AS rental_object
-        WHERE
-            c.college @> '{"name": "{ prefs["campus"] or "Texas A&M University" }"}'
-            AND (rental_object->>'rent')::int <= { prefs["max_rent"] or 5000 }
-            AND (rental_object->>'squareFeet')::int >= { prefs["min_sqft"] or 0 }]
-        ORDER BY weighted_score ASC
-        LIMIT 50
-        OFFSET ({ prefs["page"] } - 1) * 50;
-    """
+    prefs = {
+        'rent_weight': prefs['rent_weight'],
+        'sqft_weight': prefs['sqft_weight'],
+        'miles_weight': prefs['miles_weight'],
+    }
 
-    result = supabase.sql(query).execute()
+    result = supabase.rpc("get_recs", prefs).execute()
 
     return result.data
 
@@ -47,6 +29,8 @@ def get_prefs_query(id):
     preferences = result.data[0]['preferences']
 
     return preferences
+
+print(get_recs_query(get_prefs_query(5)))
 
 # Execute the raw SQL query
 @app.route('/get_recommendations', methods=['GET'])
